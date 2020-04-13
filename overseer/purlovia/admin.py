e975e70c90fdf2af31fa507ccbb583d0e2131335
Christopher Bailey <cbailey@mort.is>
2020-04-13 19:52:04 -0400

    Styling

--- overseer/purlovia/admin.py
+++ overseer/purlovia/admin.py
@@ -1,4 +1,5 @@
 from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 from django import forms
 from django.conf.urls import url
 from django.contrib import admin, messages
@@ -27,6 +28,8 @@ class ConfigRevisionForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
+
+        self.helper.add_input(Submit("submit", "Save"))
 
 
 class ConfigForm(ConfigRevisionForm):
